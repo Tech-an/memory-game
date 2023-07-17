2aeb142381b605c4f9ae3ddfad85fce2127984f9
ak-sato-tetsuya <sato_tetsuya@akconsulting.co.jp>
2023-07-17 16:35:43 +0900

    pathのエラー解消

--- src/components/card_state.ts
+++ src/components/card_state.ts
@@ -1,5 +1,5 @@
 import { atom } from 'recoil';
-import { CardProps } from "@/components/sato2/card_type";
+import { CardProps } from "@/components/card_type";
 
 const marks = ["hearts", "diamonds", "spades", "clubs"];
 const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13];
